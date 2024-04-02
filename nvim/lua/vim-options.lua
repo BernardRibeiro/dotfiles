aa8458f0c5be7f8287d70982b25a9d7e3cccf359
BernardRibeiro <bernard.ribeiro@gmail.com>
2024-04-02 19:16:25 -0300

    added nvim files

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
+
+vim.opt.swapfile = false
+
+vim.lsp.set_log_level("debug")
+
+vim.wo.number = true
